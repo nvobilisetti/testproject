cfd1741579d95e793c8e484aeec6589f89951ff4
vobilisetti <vobilisetti@gmail.com>
2018-08-17 09:50:57 -0400

    Support added to the new feature

--- src/main/java/com/nareen/Feature1.java
+++ src/main/java/com/nareen/Feature1.java
@@ -5,4 +5,8 @@ public class Feature1 {
     public void featureOne(){
         System.out.println("new feature added in branch1");
     }
+
+    public void addSupportTofeatureOne(){
+        System.out.println("added the support to the feature one");
+    }
 }
