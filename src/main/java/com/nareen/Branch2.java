d729bf7c065d99cd916f38676dc7fd1aa8075615
vobilisetti <vobilisetti@gmail.com>
2018-08-17 10:08:46 -0400

    support to contact validations

--- src/main/java/com/nareen/Branch2.java
+++ src/main/java/com/nareen/Branch2.java
@@ -5,4 +5,8 @@ public class Branch2 {
     public void getContacts(){
         System.out.println("Added feature to get contacts");
     }
+
+    public void contactValidation(){
+        System.out.println("support to validate contacts");
+    }
 }
